76bba4bdf17b565df106788a5bb8c1be71a7e041
Abhishek <abhishekasks151@gmail.com>
2025-07-21 11:15:12 +0530

    Fixed CORS-Header

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,13 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'api'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",     # React app
+#     "http://127.0.0.1:8000",
+#     "https://yourdomain.com",
+# ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
